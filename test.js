c6a5d348ec55b0cabfe091d381d70db2c9bcf8be
Tarit Mahato <122891749+mahatotarit@users.noreply.github.com>
2024-03-25 16:19:26 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // Replace 'YOUR_TELEGRAM_BOT_TOKEN' with your actual bot token obtained from BotFather
-const token = '6458087750:AAHfey42yyHAJk3lmXb12XJCOeQlf9u3x7M';
+const token = '';
 
 // Create a bot instance
 const bot = new TelegramBot(token, { polling: true });
